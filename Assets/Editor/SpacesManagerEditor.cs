2183be9d1f2ad0ec00b9cb47e23e717d7861bc2b
Sebastian <jsebdev@gmail.com>
2023-07-21 20:37:18 -0500

    Making movement coroutine stop when other movement is started

--- Assets/Editor/SpacesManagerEditor.cs
+++ Assets/Editor/SpacesManagerEditor.cs
@@ -15,9 +15,7 @@ namespace ReUpVirtualTwin
             GameObject[] spaces = GameObject.FindGameObjectsWithTag(TagsEnum.spaceSelector);
             if (spaces.Length != spacesManager.spaceSelectors.Count)
             {
-                Debug.Log($"something is going on ... currently there are {spacesManager.spaceSelectors.Count}");
                 spacesManager.UpdateSpaces();
-                Debug.Log($"after something going on ... the number is {spacesManager.spaceSelectors.Count}");
             }
             EditorGUILayout.LabelField("List of spaces in the scene: ", EditorStyles.boldLabel);
             foreach (SpaceSelector spaceSelector in spacesManager.spaceSelectors)
