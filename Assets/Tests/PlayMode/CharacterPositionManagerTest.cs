bc519bddf92511557e291da1b1a29d17bc46ca8d
Sebastian <jsebdev@gmail.com>
2023-08-16 16:51:21 -0500

    Make second test for character position manager

--- Assets/Tests/PlayMode/CharacterPositionManagerTest.cs
+++ Assets/Tests/PlayMode/CharacterPositionManagerTest.cs
@@ -16,13 +16,36 @@ public class CharacterPositionManagerTest : MonoBehaviour
         //check original character's position
         Assert.AreEqual(Vector3.zero, character.transform.position);
 
-        //var targetPosition = new Vector3(10, 10, 10);
-        var targetPosition = new Vector3(10, 0, 0);
+        var targetPosition = new Vector3(10, 10, 10);
 
         posManager.SliceToTarget(targetPosition);
 
         yield return new WaitForSeconds(5);
 
-        Assert.LessOrEqual(Vector3.Distance(targetPosition, character.transform.position), 0.5f);
+        //check new character's position is REALLY close to target position
+        Assert.LessOrEqual(Vector3.Distance(targetPosition, character.transform.position), 0.1f);
+    }
+
+    [UnityTest]
+    public IEnumerator WalkToTarget_should_success()
+    {
+        var character = new GameObject();
+        var rb = character.AddComponent<Rigidbody>();
+        rb.useGravity = false;
+        var posManager = character.AddComponent<CharacterPositionManager>();
+
+        //check original character's position
+        Assert.AreEqual(Vector3.zero, character.transform.position);
+
+        var targetPosition = new Vector3(10, 10, 10);
+
+        posManager.WalkToTarget(targetPosition);
+
+        yield return new WaitForSeconds(5);
+
+        var sameHeightTargetPosition = new Vector3(10, 0, 10);
+
+        //check new character's position is close to target position
+        Assert.LessOrEqual(Vector3.Distance(sameHeightTargetPosition, character.transform.position), 0.5f);
     }
 }
