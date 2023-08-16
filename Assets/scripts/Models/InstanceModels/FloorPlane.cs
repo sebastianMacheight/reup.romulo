b1bfd1b7ce4a7e75a6d787694925d82f0acdd479
Sebastian <jsebdev@gmail.com>
2023-08-16 11:30:40 -0500

    Rename wrong namespace

--- Assets/scripts/Models/InstanceModels/FloorPlane.cs
+++ Assets/scripts/Models/InstanceModels/FloorPlane.cs
@@ -1,4 +1,4 @@
-using ReupVirtualTwinMain;
+using ReupVirtualTwin;
 using UnityEngine;
 
 public class FloorPlane : SpaceObjectWithGizmo
@@ -14,12 +14,12 @@ public class FloorPlane : SpaceObjectWithGizmo
         {
             Gizmos.color = new Color(0.05f, 0.5f, 0.2f);
             var l = _spacesManager.floorPlanesLength;
-            float gridSize = (float) 1 / (11 - _spacesManager.gizmoGridSize);
+            float gridSize = (float)1 / (11 - _spacesManager.gizmoGridSize);
             for (float i = 0f; i < l; i = i + gridSize)
             {
                 for (float j = 0f; j < l; j = j + gridSize)
                 {
-                    Gizmos.DrawWireCube(transform.position - new Vector3(i - l/2, 0 ,j - l/2), new Vector3(1,0,1));
+                    Gizmos.DrawWireCube(transform.position - new Vector3(i - l / 2, 0, j - l / 2), new Vector3(1, 0, 1));
                 }
             }
         }
