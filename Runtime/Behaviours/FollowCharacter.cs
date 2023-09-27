2ec04d6917b376c7fa1f32f0e567df7e6e41115b
Sebastian <jsebdev@gmail.com>
2023-09-27 16:07:23 -0500

    Add follow character behaviour

---
+++ Runtime/Behaviours/FollowCharacter.cs
@@ -0,0 +1,19 @@
+using ReupVirtualTwin.helpers;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCharacter : MonoBehaviour
+{
+    GameObject _character;
+    void Start()
+    {
+        _character = ObjectFinder.FindCharacter();
+    }
+
+    void Update()
+    {
+        Vector3 lookDirection = _character.transform.position - transform.position;
+        transform.rotation = Quaternion.LookRotation(lookDirection);
+    }
+}
