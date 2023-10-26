42c4d6eebf25637b4a401e27f8ee8be98d1887d9
Sebastian <jsebdev@gmail.com>
2023-10-26 18:20:32 -0500

    Clean collision dector

--- Runtime/Behaviours/DetectCollision.cs
+++ Runtime/Behaviours/DetectCollision.cs
@@ -8,7 +8,6 @@ namespace ReupVirtualTwin.behaviours
     public class DetectCollision : MonoBehaviour
     {
         CharacterPositionManager _positionManager;
-        bool bouncing = false;
 
         private void Start()
         {
@@ -28,39 +27,12 @@ namespace ReupVirtualTwin.behaviours
             }
             bounceDirection.y = 0;
             Debug.DrawRay(_positionManager.characterPosition, bounceDirection, Color.red, 100);
-            //Debug.Log($"bounce direction: {bounceDirection}");
-            bouncing = true;
             _positionManager.MovePositionByStepInDirection(bounceDirection);
             _positionManager.ApplyForceInDirection(bounceDirection);
-            //_positionManager.MakeKinematic();
         }
-
-        //private void OnCollisionStay(Collision collision)
-        //{
-        //    if (bouncing)
-        //    {
-        //        return;
-        //    }
-        //    Debug.Log("stay the collision");
-        //    //Debug.Log($"there are {collision.contacts.Length} collision");
-        //    var bounceDirection = Vector3.zero;
-        //    foreach (ContactPoint contact in collision.contacts)
-        //    {
-        //        bounceDirection += contact.normal;
-        //    }
-        //    bounceDirection.y = 0;
-        //    Debug.DrawRay(_positionManager.characterPosition, bounceDirection, Color.red, 100);
-        //    //Debug.Log($"bounce direction: {bounceDirection}");
-        //    bouncing = true;
-        //    _positionManager.MovePositionByStepInDirection(bounceDirection);
-        //    //_positionManager.MakeKinematic();
-        //}
 
         private void OnCollisionExit()
         {
-            //Debug.Log("exit the collision");
-            //bouncing = false;
-            //_positionManager.UndoKinematic();
             _positionManager.allowSetHeight = true;
             _positionManager.allowWalking = true;
         }
