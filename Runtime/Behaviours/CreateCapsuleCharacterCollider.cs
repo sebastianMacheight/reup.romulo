41790dfe15b24aee9812506418e77e2ad616a5b0
Sebastian <jsebdev@gmail.com>
2023-10-27 05:10:29 -0500

    Precalculate height of space before jumping

--- Runtime/Behaviours/CreateCapsuleCharacterCollider.cs
+++ Runtime/Behaviours/CreateCapsuleCharacterCollider.cs
@@ -8,8 +8,6 @@ namespace ReupVirtualTwin.behaviours
         [SerializeField]
         GameObject character;
         [SerializeField]
-        HeightKeeper heightKeeper;
-        [SerializeField]
         PhysicMaterial physicMaterial;
 
         float FREE_SPACE_BELOW_IN_METERS = 0.4f;
@@ -18,7 +16,7 @@ namespace ReupVirtualTwin.behaviours
         private void Start()
         {
             var collider = character.AddComponent<CapsuleCollider>();
-            var colliderHeight = heightKeeper.CHARACTER_HEIGHT - FREE_SPACE_BELOW_IN_METERS;
+            var colliderHeight = HeightKeeper.CHARACTER_HEIGHT - FREE_SPACE_BELOW_IN_METERS;
             var colliderYCenter = -1 * colliderHeight / 2;
             collider.radius = COLLIDER_RADIUS_IN_METERS;
             collider.height = colliderHeight;
