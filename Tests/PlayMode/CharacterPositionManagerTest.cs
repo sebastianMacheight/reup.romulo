bd8e2b45da98da0a546b2a8d7286100cad70602d
Sebastian <jsebdev@gmail.com>
2023-10-31 09:02:08 -0500

    More position manager tests

--- Tests/PlayMode/CharacterPositionManagerTest.cs
+++ Tests/PlayMode/CharacterPositionManagerTest.cs
@@ -10,6 +10,10 @@ public class CharacterPositionManagerTest : MonoBehaviour
     private Rigidbody rb;
     private CharacterPositionManager posManager;
 
+    float HEIGHT_CLOSENESS_THRESHOLD = 0.02f;
+    float MOVEMENT_CLOSENESS_THRESHOLD = 0.02f;
+    float WALK_CLOSENESS_THRESHOLD = 0.5f;
+
     [SetUp]
     public void SetUp()
     {
@@ -17,16 +21,17 @@ public class CharacterPositionManagerTest : MonoBehaviour
         rb = character.AddComponent<Rigidbody>();
         rb.useGravity = false;
         posManager = character.AddComponent<CharacterPositionManager>();
+        posManager.maxStepHeight = 0.25f;
     }
 
     [TearDown]
     public void TearDown()
     {
-        Object.Destroy(character);
+        Destroy(character);
     }
 
     [UnityTest]
-    public IEnumerator SliceToTargetShouldSuccess()
+    public IEnumerator SlideToTargetShouldSuccess()
     {
         //check original character's position
         Assert.AreEqual(Vector3.zero, character.transform.position);
@@ -38,7 +43,7 @@ public class CharacterPositionManagerTest : MonoBehaviour
         yield return new WaitForSeconds(3);
 
         //check new character's position is REALLY close to target position
-        Assert.LessOrEqual(Vector3.Distance(targetPosition, character.transform.position), 0.1f);
+        Assert.LessOrEqual(Vector3.Distance(targetPosition, character.transform.position), MOVEMENT_CLOSENESS_THRESHOLD);
     }
 
     [UnityTest]
@@ -52,11 +57,71 @@ public class CharacterPositionManagerTest : MonoBehaviour
         posManager.WalkToTarget(targetPosition);
 
         yield return new WaitForSeconds(6);
-        yield return null;
 
         var sameHeightTargetPosition = new Vector3(5, 0, 5);
 
         //check new character's position is close to target position
-        Assert.LessOrEqual(Vector3.Distance(sameHeightTargetPosition, character.transform.position), 0.5f);
+        Assert.LessOrEqual(Vector3.Distance(sameHeightTargetPosition, character.transform.position), WALK_CLOSENESS_THRESHOLD);
+    }
+
+    [UnityTest]
+    public IEnumerator HeightToTargetShouldSuccessIfFallingDown()
+    {
+        //check original character's position
+        Assert.AreEqual(Vector3.zero, character.transform.position);
+
+        posManager.KeepHeight(-1f);
+
+        yield return new WaitForSeconds(1);
+
+        //check new character's position is close to target position
+        var expectedPosition = new Vector3(0, -1, 0);
+        Assert.LessOrEqual(Vector3.Distance(character.transform.position, expectedPosition), HEIGHT_CLOSENESS_THRESHOLD);
+    }
+
+    [UnityTest]
+    public IEnumerator HeightToTargetShouldSuccessIfStepingUp()
+    {
+        //check original character's position
+        Assert.AreEqual(Vector3.zero, character.transform.position);
+
+        posManager.KeepHeight(0.20f);
+
+        yield return new WaitForSeconds(1);
+
+        //check new character's position is close to target position
+        var expectedPosition = new Vector3(0, 0.2f, 0);
+        Assert.LessOrEqual(Vector3.Distance(character.transform.position, expectedPosition), HEIGHT_CLOSENESS_THRESHOLD);
+    }
+
+    [UnityTest]
+    public IEnumerator HeightToTargetShouldFailIfStepingTooMuchUp()
+    {
+        //check original character's position
+        Assert.AreEqual(Vector3.zero, character.transform.position);
+
+        posManager.KeepHeight(0.35f);
+
+        yield return new WaitForSeconds(1);
+
+        //check new character's position is close to target position
+        var expectedPosition = Vector3.zero;
+        Assert.LessOrEqual(Vector3.Distance(character.transform.position, expectedPosition), HEIGHT_CLOSENESS_THRESHOLD);
+    }
+    [UnityTest]
+    public IEnumerator MoveDistanceInDirectionShouldSuccess()
+    {
+        //check original character's position
+        Assert.AreEqual(Vector3.zero, character.transform.position);
+
+        var direction = new Vector3(1, 1, 2);
+        posManager.MoveDistanceInDirection(2f, direction);
+
+        //check new character's position is close to target position
+        var sqrt6 = Mathf.Sqrt(6);
+        var expectedPosition = new Vector3(sqrt6, sqrt6, 2*sqrt6) / 3;
+        Assert.LessOrEqual(Vector3.Distance(character.transform.position, expectedPosition), 1E-5);
+        yield return null;
     }
 }
+
