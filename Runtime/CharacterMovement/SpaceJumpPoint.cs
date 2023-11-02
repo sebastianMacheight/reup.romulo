3e83cdf0e88159beba0ce319ce3bc8306d852f4f
Sebastian <jsebdev@gmail.com>
2023-11-02 10:11:39 -0500

    Remove draw space gizmos toggle

--- Runtime/CharacterMovement/SpaceJumpPoint.cs
+++ Runtime/CharacterMovement/SpaceJumpPoint.cs
@@ -5,16 +5,11 @@ namespace ReupVirtualTwin.characterMovement
     public class SpaceJumpPoint : MonoBehaviour
     {
         public string spaceName = "Unnamed space";
-        [HideInInspector]
-        public bool drawGizmo = true;
 
         private void OnDrawGizmos()
         {
-            if (drawGizmo)
-            {
-                Gizmos.color = Color.magenta;
-                Gizmos.DrawSphere(transform.position, 0.3f);
-            }
+            Gizmos.color = Color.magenta;
+            Gizmos.DrawSphere(transform.position, 0.3f);
         }
     }
 }
