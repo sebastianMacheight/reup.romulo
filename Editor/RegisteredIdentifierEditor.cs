13b961fd82e20cf5f6ccaaa710c28d57efe28dd2
Sebastian <jsebdev@gmail.com>
2023-11-21 15:35:23 -0500

    Set Editor for RegisteredIdentifier

--- Editor/RegisteredIdentifierEditor.cs
+++ Editor/RegisteredIdentifierEditor.cs
@@ -1,13 +1,13 @@
 using UnityEditor;
 using ReupVirtualTwin.models;
 
-[CustomEditor(typeof(UniqueId))]
-public class UniqueIdEditor : Editor
+[CustomEditor(typeof(RegisteredIdentifier))]
+public class RegisteredIdentifierEditor : Editor
 {
     public override void OnInspectorGUI()
     {
         base.OnInspectorGUI();
-        UniqueId Identifier = (UniqueId)target;
+        UniqueId Identifier = (RegisteredIdentifier)target;
         EditorGUILayout.TextField("copy and paste it!", Identifier.uniqueId);
     }
 }
