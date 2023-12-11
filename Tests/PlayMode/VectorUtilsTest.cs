1a15685ce870c0b3055654b48b41fa7caa85fde9
Sebastian <jsebdev@gmail.com>
2023-12-11 11:25:51 -0500

    Create ObjectWrapper

---
+++ Tests/PlayMode/VectorUtilsTest.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NUnit.Framework;
+using UnityEngine.TestTools;
+using ReupVirtualTwin.dataModels;
+using ReupVirtualTwin.helpers;
+
+public class VectorUtilsTest : MonoBehaviour
+{
+    [UnityTest]
+    public IEnumerator Vector3MeanWorks()
+    {
+        List<Vector3> vectors = new List<Vector3>();
+        vectors.Add(new Vector3(1,2,3));
+        vectors.Add(new Vector3(2,4,6));
+        Vector3 mean = VectorUtils.meanVector3(vectors);
+        Assert.AreEqual(new Vector3(1.5f, 3, 4.5f), mean);
+        yield return null;
+    }
+}
