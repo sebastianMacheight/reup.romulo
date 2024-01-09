7f19093090b2a785220a14d12bdf838a16be22fc
Sebastian <jsebdev@gmail.com>
2024-01-09 18:17:04 -0500

    Implement highlighter in selected objects

--- Runtime/Managers/EditModeManagerDependencyInjector.cs
+++ Runtime/Managers/EditModeManagerDependencyInjector.cs
@@ -1,4 +1,5 @@
 using ReupVirtualTwin.behaviourInterfaces;
+using ReupVirtualTwin.managerInterfaces;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,6 +9,8 @@ namespace ReupVirtualTwin.managers
     public class EditModeManagerDependencyInjector : MonoBehaviour
     {
         EditModeManager _editModeManager;
+        [SerializeField]
+        GameObject editionMediator;
         private void Awake()
         {
             _editModeManager = GetComponent<EditModeManager>();
@@ -17,6 +20,7 @@ namespace ReupVirtualTwin.managers
                 throw new System.Exception("WebMessageSender not found");
             }
             _editModeManager.webMessageSender = webMessageSender;
+            _editModeManager.mediator = editionMediator.GetComponent<IMediator>();
         }
     }
 }
