59147a4ee0222f915fa03679f4df09f556652232
Sebastian <jsebdev@gmail.com>
2024-01-10 12:05:46 -0500

    Fix EditionMediatorTests

--- Runtime/ManagerInterfaces/ISelectedObjectsManager.cs
+++ Runtime/ManagerInterfaces/ISelectedObjectsManager.cs
@@ -4,6 +4,7 @@ namespace ReupVirtualTwin.managerInterfaces
 {
     public interface ISelectedObjectsManager
     {
+        public bool allowSelection { get;  set; }
         public GameObject selection { get; }
         public void ClearSelection();
         public GameObject AddObjectToSelection(GameObject selectedObject);
