8562c9501e5e79a6f0c37ef7c74262eafbee0d30
Sebastian <jsebdev@gmail.com>
2024-01-14 20:57:14 -0500

    update web message types

--- Runtime/Enums/WebMessageType.cs
+++ Runtime/Enums/WebMessageType.cs
@@ -4,15 +4,19 @@ namespace ReupVirtualTwin.enums
     {
         public const string showMaterialsOptions = "showMaterialsOptions";
         public const string hideMaterialsOptions = "hideMaterialsOptions";
+
         public const string setEditMode = "[Edit Mode] Set Edit Mode";
         public const string setEditModeSuccess = "[Edit Mode] Set Edit Mode Success";
+
         public const string setSelectedObjects = "[Selected Objects] Set Selected Objects Success";
-        public const string activatePositionTransform = "[Transform Objects] Activate Position Transform";
-        public const string activatePositionTransformSuccess = "[Transform Objects] Activate Position Transform Success";
-        public const string activateRotationTransform = "[Transform Objects] Activate Rotation Transform";
-        public const string activateRotationTransformSuccess = "[Transform Objects] Activate Rotation Transform Success";
+
+        public const string activatePositionTransform = "[Transform Objects] Activate Position Transform Mode";
+        public const string activatePositionTransformSuccess = "[Transform Objects] Activate Position Transform Mode Success";
+        public const string activateRotationTransform = "[Transform Objects] Activate Rotation Transform Mode";
+        public const string activateRotationTransformSuccess = "[Transform Objects] Activate Rotation Transform Mode Success";
         public const string deactivateTransformMode = "[Transform Objects] Deactivate Transform Mode";
         public const string deactivateTransformModeSuccess = "[Transform Objects] Deactivate Transform Mode Success";
-        public const string error = "[Error] Engine error";
+
+        public const string error = "[Error] Engine Error";
     }
 }
