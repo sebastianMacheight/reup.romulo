6890d9677bce0a2877f5eab60a0ac47e6f9e4c41
Juanfemach <131793253+Juanfemach@users.noreply.github.com>
2024-01-17 15:37:09 -0500

    Delete unused code

--- Runtime/Utils/AddGizmo.cs
+++ Runtime/Utils/AddGizmo.cs
@@ -4,19 +4,7 @@ using UnityEngine;
 
 public class AddGizmo : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    private void OnDrawGizmos()
+      private void OnDrawGizmos()
     {
         Gizmos.color = Color.yellow;
         Gizmos.DrawSphere(transform.position, 0.05f);
