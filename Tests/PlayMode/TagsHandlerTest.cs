fe7ab1ef82f5eb565bf5ba072a0de94af6b5762a
Sebastian <jsebdev@gmail.com>
2024-01-18 15:30:42 -0500

    Fix typos

--- Tests/PlayMode/TagsHandlerTest.cs
+++ Tests/PlayMode/TagsHandlerTest.cs
@@ -12,7 +12,7 @@ public class TagsHandlerTest : MonoBehaviour
 {
     GameObject tagsHandlerContainer;
     GameObject taggedObject0;
-    GameObject tagedObject1;
+    GameObject taggedObject1;
     GameObject nonTaggedObject0;
     TagsHandler tagsHandler;
     ObjectTags objectTags0;
@@ -23,50 +23,50 @@ public class TagsHandlerTest : MonoBehaviour
     {
         tagsHandlerContainer = new GameObject("tagsHandlerContainer");
         tagsHandler = tagsHandlerContainer.AddComponent<TagsHandler>();
-        tagedObject0 = new GameObject("taggedObj0");
-        objectTags0 = tagedObject0.AddComponent<ObjectTags>();
+        taggedObject0 = new GameObject("taggedObj0");
+        objectTags0 = taggedObject0.AddComponent<ObjectTags>();
         objectTags0.AddTags(new ObjectTag[2] {ObjectTag.SELECTABLE, ObjectTag.TRANSFORMABLE});
-        tagedObject1 = new GameObject("taggedObject1");
-        objectTags1 = tagedObject1.AddComponent<ObjectTags>();
+        taggedObject1 = new GameObject("taggedObject1");
+        objectTags1 = taggedObject1.AddComponent<ObjectTags>();
         objectTags1.AddTag(ObjectTag.SELECTABLE);
         nonTaggedObject0 = new GameObject("nonTaggedObj0");
     }
     [TearDown]
     public void TearDown()
     {
-        Destroy(tagedObject0);
+        Destroy(taggedObject0);
     }
     [UnityTest]
     public IEnumerator ShouldReturnFalseOnCheckForTags()
     {
-        Assert.IsFalse(tagsHandler.DoesObjectHaveTag(tagedObject0, ObjectTag.DELETABLE));
-        Assert.IsFalse(tagsHandler.DoesObjectHaveTag(tagedObject1, ObjectTag.DELETABLE));
+        Assert.IsFalse(tagsHandler.DoesObjectHaveTag(taggedObject0, ObjectTag.DELETABLE));
+        Assert.IsFalse(tagsHandler.DoesObjectHaveTag(taggedObject1, ObjectTag.DELETABLE));
         Assert.IsFalse(tagsHandler.DoesObjectHaveTag(nonTaggedObject0, ObjectTag.DELETABLE));
         yield return null;
     }
     [UnityTest]
     public IEnumerator ShouldReturnTrueOnCheckForTags()
     {
-        Assert.IsTrue(tagsHandler.DoesObjectHaveTag(tagedObject0, ObjectTag.SELECTABLE));
-        Assert.IsTrue(tagsHandler.DoesObjectHaveTag(tagedObject1, ObjectTag.SELECTABLE));
+        Assert.IsTrue(tagsHandler.DoesObjectHaveTag(taggedObject0, ObjectTag.SELECTABLE));
+        Assert.IsTrue(tagsHandler.DoesObjectHaveTag(taggedObject1, ObjectTag.SELECTABLE));
         yield return null;
     }
     [UnityTest]
     public IEnumerator ShouldAddTags()
     {
-        Assert.IsFalse(tagsHandler.DoesObjectHaveTag(tagedObject0, ObjectTag.DELETABLE));
-        tagsHandler.AddTagToObject(tagedObject0, ObjectTag.DELETABLE);
+        Assert.IsFalse(tagsHandler.DoesObjectHaveTag(taggedObject0, ObjectTag.DELETABLE));
+        tagsHandler.AddTagToObject(taggedObject0, ObjectTag.DELETABLE);
         yield return null;
-        Assert.IsTrue(tagsHandler.DoesObjectHaveTag(tagedObject0, ObjectTag.DELETABLE));
+        Assert.IsTrue(tagsHandler.DoesObjectHaveTag(taggedObject0, ObjectTag.DELETABLE));
         yield return null;
     }
     [UnityTest]
     public IEnumerator ShouldRemoveTags()
     {
-        Assert.IsTrue(tagsHandler.DoesObjectHaveTag(tagedObject0, ObjectTag.SELECTABLE));
-        tagsHandler.RemoveTagFromOjbect(tagedObject0, ObjectTag.SELECTABLE);
+        Assert.IsTrue(tagsHandler.DoesObjectHaveTag(taggedObject0, ObjectTag.SELECTABLE));
+        tagsHandler.RemoveTagFromOjbect(taggedObject0, ObjectTag.SELECTABLE);
         yield return null;
-        Assert.IsFalse(tagsHandler.DoesObjectHaveTag(tagedObject0, ObjectTag.SELECTABLE));
+        Assert.IsFalse(tagsHandler.DoesObjectHaveTag(taggedObject0, ObjectTag.SELECTABLE));
         yield return null;
     }
 
