00ff6246bde8ae194652cba1505172f446c24d54
Sebastian <jsebdev@gmail.com>
2024-01-19 16:38:06 -0500

    Create IIdController interface

---
+++ Runtime/ControllersInterfaces/IIdGetterController.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace ReupVirtualTwin.controllerInterfaces
+{
+    public interface IIdGetterController
+    {
+        public string GetIdFromObject(GameObject obj);
+    }
+}
