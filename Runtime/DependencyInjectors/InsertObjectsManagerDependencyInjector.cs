f029da3c758bfd5a204046d54e46f6d6adfba90e
Sebastian <jsebdev@gmail.com>
2024-01-19 18:07:08 -0500

    Comunicate insert manager with mediator

--- Runtime/DependencyInjectors/InsertObjectsManagerDependencyInjector.cs
+++ Runtime/DependencyInjectors/InsertObjectsManagerDependencyInjector.cs
@@ -1,5 +1,6 @@
 using ReupVirtualTwin.controllers;
 using ReupVirtualTwin.helpers;
+using ReupVirtualTwin.managerInterfaces;
 using ReupVirtualTwin.managers;
 using System.Collections;
 using System.Collections.Generic;
@@ -9,12 +10,14 @@ namespace ReupVirtualTwin.dependencyInjectors
 {
     public class InsertObjectsManagerDependencyInjector : MonoBehaviour
     {
+        public GameObject mediator;
         private void Awake()
         {
             InsertObjectsManager insertObjectsManager = GetComponent<InsertObjectsManager>();
             insertObjectsManager.colliderAdder = new ColliderAdder();
             insertObjectsManager.tagSystemController = new TagSystemController();
             insertObjectsManager.idAssigner = new IdController();
+            insertObjectsManager.mediator = mediator.GetComponent<IMediator>();
         }
     }
 }
