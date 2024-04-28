be9f695835cd347a80a04b8f551e3a680a6d40ed
Sebastian <jsebdev@gmail.com>
2024-04-27 22:07:42 -0500

    remove failing test

--- Tests/PlayMode/VectorUtilsTest.cs
+++ Tests/PlayMode/VectorUtilsTest.cs
@@ -18,15 +18,4 @@ public class VectorUtilsTest : MonoBehaviour
         yield return null;
     }
 
-    [UnityTest]
-    public IEnumerator Vector3MeanWorksFails()
-    {
-        List<Vector3> vectors = new List<Vector3>();
-        vectors.Add(new Vector3(1,2,3));
-        vectors.Add(new Vector3(2,4,6));
-        Vector3 mean = VectorUtils.meanVector3(vectors);
-        Assert.AreEqual(new Vector3(1.5f, 3, 5.0f), mean);
-        yield return null;
-    }
-
 }
